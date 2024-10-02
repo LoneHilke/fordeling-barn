5a145e2bbb9f16cfdc7837c3c82b8d906e895e54
LMH <90649822+LoneHilke@users.noreply.github.com>
2024-10-02 18:08:53 +0200

    videre
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- barn/barn/klasse/models.py
+++ barn/barn/klasse/models.py
@@ -5,9 +5,10 @@ class Barn(models.Model):
     navn = models.CharField(max_length=50)
     klasse = models.ManyToManyField('Klasse', related_name='item')
     niveau = models.Choices()
+    klas_trin = models.CharField(max_length=10)
 
 class Personale(models.Model):
     navn = models.CharField(max_length=100)
     klasse = models.ManyToManyField('Klasse', related_name='item')
     stilling = models.CharField(max_length=50)
-    
+    timer = models.CharField(max_length=10)
---
+++ barn/barn/klasse/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from views import Base
+urlpatterns = [
+    
+    path('', Base.as_view, name='base'),
+]
